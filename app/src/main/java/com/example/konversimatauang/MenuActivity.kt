6629ebbd14ddd51fda9e296d04c32d1d0b0bbd2f
Itsyogz <thisyogz3@gmail.com>
2024-05-18 16:54:08 +0700

    Fragment update!

---
+++ app/src/main/java/com/example/konversimatauang/MenuActivity.kt
@@ -0,0 +1,33 @@
+package com.example.konversimatauang
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MenuActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.fragmentContainerView
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/konversimatauang/OnboardingItem.kt
@@ -0,0 +1,7 @@
+package com.example.konversimatauang
+
+data class OnboardingItem(
+    val onboardingImage: Int,
+    val title: String,
+    val description: String
+)
