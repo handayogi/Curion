c6b215b19ec38297dbb2333200663ba6ddaa9457
Itsyogz <thisyogz3@gmail.com>
2024-05-20 05:12:19 +0700

    convert update

--- app/src/main/java/com/example/konversimatauang/ConvertFragment.kt
+++ app/src/main/java/com/example/konversimatauang/ConvertFragment.kt
@@ -1,59 +1,143 @@
 package com.example.konversimatauang
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ConvertFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConvertFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var baseCurrency = "USD"
+    var convertedToCurrency = "IDR"
+    var conversionRate = 0f
+
+    private lateinit var editFromCurrency: EditText
+    private lateinit var editToCurrency: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_convert, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConvertFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConvertFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        editFromCurrency = view.findViewById(R.id.editFromCurrency)
+        editToCurrency = view.findViewById(R.id.editToCurrency)
+        spinnerSetup(view)
+        textChangedStuff()
+    }
+
+    private fun textChangedStuff() {
+        editFromCurrency.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null && s.isNotEmpty()) {
+                    getApiResult()
                 }
             }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("Main", "Before Text Changed")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("Main", "On Text Changed")
+            }
+        })
+    }
+
+    private fun getApiResult() {
+        if (editFromCurrency.text.isNotEmpty() && editFromCurrency.text.isNotBlank()) {
+            val api = "https://api.currencyapi.com/v3/latest?apikey=cur_live_jPaP565zswh8xCtGhHHgE5eTt9o30v0p96cu0Tfd"
+
+            if (baseCurrency == convertedToCurrency) {
+                Toast.makeText(context, "Please pick a currency to convert", Toast.LENGTH_SHORT).show()
+            } else {
+                lifecycleScope.launch(Dispatchers.IO) {
+                    try {
+                        val apiResult = URL(api).readText()
+                        val jsonObject = JSONObject(apiResult)
+                        conversionRate = jsonObject.getJSONObject("data").getJSONObject(convertedToCurrency).getString("value").toFloat()
+
+                        Log.d("Main", "$conversionRate")
+                        Log.d("Main", apiResult)
+
+                        withContext(Dispatchers.Main) {
+                            val text = ((editFromCurrency.text.toString().toFloat()) * conversionRate).toString()
+                            editToCurrency.setText(text)
+                        }
+                    } catch (e: Exception) {
+                        Log.e("Main", "ISSUE: $e")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun spinnerSetup(view: View) {
+        val firstSpinner: Spinner = view.findViewById(R.id.spFromCurrency)
+        val secondSpinner: Spinner = view.findViewById(R.id.spToCurrency)
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.firstCurrency_code,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            firstSpinner.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.secondCurrency_code,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            secondSpinner.adapter = adapter
+        }
+
+        firstSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                baseCurrency = parent?.getItemAtPosition(position).toString()
+                getApiResult()
+            }
+        }
+
+        secondSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                convertedToCurrency = parent?.getItemAtPosition(position).toString()
+                getApiResult()
+            }
+        }
     }
 }
