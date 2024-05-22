869e909149165d93628538ae0981f74a3bf156ad
Itsyogz <thisyogz3@gmail.com>
2024-05-23 00:30:19 +0700

    Menu Home update!

--- app/src/main/java/com/example/konversimatauang/HomeFragment.kt
+++ app/src/main/java/com/example/konversimatauang/HomeFragment.kt
@@ -29,8 +29,16 @@ class HomeFragment : Fragment() {
                 Color.parseColor("#81FFFFFF"),
                 Color.TRANSPARENT
             )
+            lineChart2.gradientFillColors = intArrayOf(
+                Color.parseColor("#81FFFFFF"),
+                Color.TRANSPARENT
+            )
+
             lineChart.animation.duration = animationDuration
             lineChart.animate(lineSet)
+
+            lineChart2.animation.duration = animationDuration
+            lineChart2.animate(lineSet2)
         }
     }
 
@@ -53,6 +61,21 @@ class HomeFragment : Fragment() {
             "label10" to 6.5f,
             "label11" to 3f,
             "label12" to 4f
+        )
+
+        private val lineSet2 = listOf(
+            "label1" to 6f,
+            "label2" to 4.5f,
+            "label3" to 7.7f,
+            "label4" to 3.5f,
+            "label5" to 5.6f,
+            "label6" to 7.5f,
+            "label7" to 7.5f,
+            "label8" to 10f,
+            "label9" to 5f,
+            "label10" to 6.5f,
+            "label11" to 3f,
+            "label12" to 7f
         )
 
         private const val animationDuration = 1000L
