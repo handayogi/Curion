e0d9b651e18975bc0406d01c86b83ca5ea4e08ec
Itsyogz <thisyogz3@gmail.com>
2024-05-23 00:36:29 +0700

    Menu Home update!

--- app/src/main/java/com/example/konversimatauang/HomeFragment.kt
+++ app/src/main/java/com/example/konversimatauang/HomeFragment.kt
@@ -49,18 +49,18 @@ class HomeFragment : Fragment() {
 
     companion object {
         private val lineSet = listOf(
-            "label1" to 5f,
-            "label2" to 4.5f,
-            "label3" to 4.7f,
-            "label4" to 3.5f,
-            "label5" to 3.6f,
-            "label6" to 7.5f,
+            "label1" to 5.994f,
+            "label2" to 5.994f,
+            "label3" to 5.994f,
+            "label4" to 5.992f,
+            "label5" to 5.996f,
+            "label6" to 6.2f,
             "label7" to 7.5f,
-            "label8" to 10f,
-            "label9" to 5f,
+            "label8" to 5.995f,
+            "label9" to 6f,
             "label10" to 6.5f,
-            "label11" to 3f,
-            "label12" to 4f
+            "label11" to 5.992f,
+            "label12" to 5.992f
         )
 
         private val lineSet2 = listOf(
